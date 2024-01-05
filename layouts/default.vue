a8d20badb38af9b814c2c3f6e002db2fbde21ab8
aldnnnz <sayabukanrobot999@gmail.com>
2024-01-05 15:20:54 +0700

    setup nuxt.js

---
+++ layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideDrawer" fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in sideMenu"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-bottom-navigation 
+        horizontal 
+        height="10vh" 
+        fixed color="primary"
+        app
+      >
+      <v-app-bar-nav-icon 
+        @click.stop="sideDrawer = !sideDrawer"
+        v-ripple="false"
+        plain 
+      />
+
+      <v-btn v-for="(item, i) in bottomMenu" 
+          :key="i" 
+          :to="item.to"
+          v-ripple="false"
+          plain>
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+      <v-spacer />
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      sideDrawer: false,
+      sideMenu: [
+        {
+          icon: "mdi-account",
+          title: "Account",
+          to: "/account",
+        },
+        {
+          icon: "mdi-bell",
+          title: "Notification",
+          to: "/notification",
+        },
+      ],
+      bottomMenu: [
+        {
+          icon: "mdi-application",
+          title: "App",
+          to: "/",
+        }
+      ],
+      right: true,
+      rightDrawer: false,
+      title: "Kacir",
+    };
+  },
+};
+</script>
